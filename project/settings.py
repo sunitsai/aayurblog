9764c8cd810dc59327b2896073ef170d07e2264f
sunitsai <58470631+sunitsai@users.noreply.github.com>
2021-03-16 22:32:12 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'y03gpy4^7debybrc7up90(=4&!xyy(_-mz(ipwf2msj^2xbwi1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aayurblog.herokuapp.com']
 
 
 # Application definition
